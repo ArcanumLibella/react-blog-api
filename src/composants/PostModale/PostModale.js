eb6415613d1635633eb5cf5f3f89ab56427cd797
ArcanumLibella <fiona.roux@hetic.net>
2021-03-09 19:09:28 +0100

    feat: add content to postModale on click

--- src/composants/PostModale/PostModale.js
+++ src/composants/PostModale/PostModale.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 import './PostModale.css'
 
 class PostModale extends Component {
+    state = {
+        loadedPost: null
+    }
+
+    componentDidUpdate() {
+        console.log('UPDATED')
+
+        if(this.props.id) {
+            if (!this.state.loadedPost || (this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => {
+                    this.setState({
+                        loadedPost: response.data
+                    })
+                })
+            }
+        }
+    }
+
     render () {
         return (
-            this.props.id ?
+            this.state.loadedPost ?
                 <div className="fullPost relative mx-auto p-8 w-4/5 lg:w-full bg-white shadow-lg rounded text-center max-w-screen-sm">
-                    <h1 className="post__title mb-4 text-xl font-black uppercase">Titre</h1>
-                    <p>Contenu</p>
+                    <h1 className="post__title mb-4 text-xl font-black uppercase">{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
             
                     <FontAwesomeIcon 
                         icon={faTimes} 
